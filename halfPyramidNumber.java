06731c4ba3bbdee4d0e13c3eaffb88d31d76abaf
soumita das <dassoumita939@gmail,com>
2026-08-12 13:09:00 +0530

    add halfPyramidNumber

---
+++ halfPyramidNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class index{
+    
+    void pattern(int a){
+            for(int i=0;i<a;i++){
+            for(int j=0; j<i+1; j++){
+                System.out.print(j+1);
+            } 
+            System.out.println();
+        }
+    }
+}
+class halfPyramidNumber{
+    public static void main(String[] args) {
+        Scanner a1= new Scanner(System.in);
+        System.out.print("enter row: ");
+        int a= a1.nextInt();
+        
+        index ind= new index();
+        ind.pattern(a);
+
+        a1.close();
+    }
+}
